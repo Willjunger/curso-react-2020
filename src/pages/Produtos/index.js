8a445b2127cf8990692c0bef3afc2ccf7894ecb3
Willjunger <williamjunger@hotmail.com>
2020-07-20 16:43:47 -0300

    first commit

---
+++ src/pages/Produtos/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class CadastroProduto extends Component {
+	render() {
+		return (
+			<div className="card">
+				<div className="card-header">Cadastro de Produtos</div>
+				<div className="card-body"></div>
+			</div>
+		);
+	}
+}
